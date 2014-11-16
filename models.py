4c4c4e89a3162e7dba8ad10c1b7171f87195eef5
Gautier Hayoun <ghayoun@gmail.com>
2014-11-16 18:01:33 +0000

    Add currency support

--- models.py
+++ models.py
@@ -1,7 +1,8 @@
 class Customer:
-    def charge(self, amount, description):
+    def charge(self, amount, description, currency):
         response = stripe.Charge.create(
             amount=100 * amount,
+            currency=curency,
             customer=self.id,
             description=description,
         )
