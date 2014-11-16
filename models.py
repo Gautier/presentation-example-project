07c37234eb6aade34f5614aa09c1567be4962b68
Gautier Hayoun <ghayoun@gmail.com>
2014-11-16 18:01:50 +0000

    Oups, fix error

--- models.py
+++ models.py
@@ -2,7 +2,7 @@ class Customer:
     def charge(self, amount, description, currency):
         response = stripe.Charge.create(
             amount=100 * amount,
-            currency=curency,
+            currency=currency,
             customer=self.id,
             description=description,
         )
